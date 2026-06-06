4538146b22854d7dd2e28d51916c0836b919f435
Martin Marshall <law@martinmarshall.com>
2026-06-06 13:16:04 -0400

    Finish converting defvar forms to defcustom

--- electric-sentence.el
+++ electric-sentence.el
@@ -30,32 +30,37 @@
   :group 'electricity
   :prefix "electric-sentence")
 
-(defvar electric-sentence-end-after-quote t
-  "Whether to add an extra space after quotes.") ; boolean
+(defcustom electric-sentence-end-after-quote t
+  "Whether to add an extra space after quotes."
+  :type '(boolean))
 
-(defvar electric-sentence-end-quotes '(?\" ?”)
+(defcustom electric-sentence-end-quotes '(?\" ?”)
   "List of close-quote characters.
 This only has effect if `electric-sentence-end-after-quote' is
-non-nil.")                              ; list of chars
+non-nil."
+  :type '(repeat character))
 
-(defvar electric-sentence-3rd-space-warning
+(defcustom electric-sentence-3rd-space-warning
   "2 spaces already found after last sentence!"
   "The warning message to issue when a 3rd space is inserted.
 Only affects 3rd space after a sentence-ending, not otherwise.  Set to
-nil for no warning message.") ; string or nil
+nil for no warning message."
+  :type '(string))
 
-(defvar electric-sentence-3rd-space-inhibit t
+(defcustom electric-sentence-3rd-space-inhibit t
   "Whether to prevent insertion of a 3rd space after a sentence.
 This only affects the 3rd space after a sentence-ending, and not
 otherwise.  Thus, if this option is enabled, you can still escape from
-the limitation by typing \\[quoted-insert] \`SPC' just once.") ; boolean
+the limitation by typing \\[quoted-insert] \`SPC' just once."
+  :type '(boolean))
 
-(defvar electric-sentence-enders '(?? ?! ?.)
-  "List of characters we consider as ending a sentence.") ; char list
+(defcustom electric-sentence-enders '(?? ?! ?.)
+  "List of characters we consider as ending a sentence."
+  :type '(repeat character))
 
 (defcustom electric-sentence-lighter " M-e"
   "Mode-line lighter for `electric-sentence-mode'."
-  :type 'string)
+  :type '(string))
 
 ;; This needs to be refactored to make it easier to add abbreviations
 ;; without editing the code.  It should be possible to use Custom to
@@ -82,7 +87,7 @@ Book contains lists of abbreviations for use in legal case titles."
   :link '(url-link
           "https://editorsmanual.com/articles/periods-in-abbreviations/"))
 
-(defvar electric-sentence-max-abbrev-length 10
+(defcustom electric-sentence-max-abbrev-length 10
   "This length includes the trailing period and possibly a prefix.
 Thus, you may need to add as much as 2 to the number of letters in the
 longest potentially-matching abbreviation in order for your longest
@@ -90,9 +95,10 @@ abbreviations to match the check.  This is used to determine how far
 back in the buffer we check for an abbreviation.  It should be an
 integer that will be long enough to catch any of the abbreviations
 matchable by `electric-sentence-abbrev-regexp'.  It is better to
-overestimate than to underestimate this.")  ; integer
+overestimate than to underestimate this."
+  :type '(number))
 
-(defvar electric-sentence-prog-modes '(prog-mode)
+(defcustom electric-sentence-prog-modes '(prog-mode)
   "List of modes in which to limit the behavior to comments and strings.
 This will take effect in any modes that have a listed mode as an
 ancestor.  The default value is a list containing `prog-mode'.  So, for
@@ -101,15 +107,17 @@ which is derived from `lisp-data-mode', which is itself derived from
 `prog-mode'.  So no change to the default value is needed for this
 setting to work in any of the foregoing modes.  However, this is a
 user-option due to the possibility of programming modes that *aren't*
-derived from `prog-mode'.")             ; list of major-mode symbols
+derived from `prog-mode'."
+  :type '(repeat symbol))
 
-(defvar electric-sentence-treat-org-src-as-prog t
+(defcustom electric-sentence-treat-org-src-as-prog t
   "Whether to treat Org-mode src-blocks like Prog-modes.
 In other words, inhibit the behavior in Org-src blocks, except for in
 comments and strings.  This makes NO distinction based on the language
 used in a src-block.  For example, with this option enabled, the
 behavior will be inhibited in Org's HTML src-blocks (except for comments
-and strings), even though HTML is not derived from `prog-mode'.") ; boolean
+and strings), even though HTML is not derived from `prog-mode'."
+  :type '(boolean))
 
 ;; Can this be better integrated with the built-in variables for
 ;; recognizing sentence endings?
